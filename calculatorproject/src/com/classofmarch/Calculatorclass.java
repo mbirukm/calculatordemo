8874b6acc5827d7acf62ee04053064267a264287
mbirukm <birukwalle@yahoo.com>
2021-04-18 22:57:57 -0400

    my first commit

---
+++ calculatorproject/src/com/classofmarch/Calculatorclass.java
@@ -0,0 +1,13 @@
+package com.classofmarch;
+
+public class Calculatorclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=12;
+int Sum =a+b;
+System.out.println(Sum);
+	}
+
+}
