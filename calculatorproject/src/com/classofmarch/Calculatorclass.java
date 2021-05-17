117788f69e1e11a58a18f90a6744e34eecc1de9b
mbirukm <birukwalle@yahoo.com>
2021-05-17 00:25:01 -0400

    coed formatted

--- calculatorproject/src/com/classofmarch/Calculatorclass.java
+++ calculatorproject/src/com/classofmarch/Calculatorclass.java
@@ -4,13 +4,14 @@ public class Calculatorclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=10;
-int b=12;
-int c=15;
-int d=40;		
-int Sum =a+c+b+d;
-System.out.println(Sum);
-System.out.println("i added 4 numbers successfully");
+		int a = 10;
+		int b = 12;
+		int c = 15;
+		int d = 40;
+
+		int Sum = a + c + b + d;
+		System.out.println(Sum);
+		System.out.println("i is added 4 numbers successfully");
 	}
 
 }
